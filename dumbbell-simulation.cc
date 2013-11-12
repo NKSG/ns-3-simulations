adbc34e149ca51f13b7a66f86c30a2bb5acec3b9
James Martin <jamesml@live.unc.edu>
2013-11-12 13:07:44 -0500

    Working simulation.

--- dumbbell-simulation.cc
+++ dumbbell-simulation.cc
@@ -15,8 +15,11 @@
  *
 */
 
+#include <cmath>
 #include <iostream>
+#include <sstream>
 
+// ns3 includes
 #include "ns3/log.h"
 #include "ns3/point-to-point-dumbbell.h"
 #include "ns3/constant-position-mobility-model.h"
@@ -24,16 +27,20 @@
 #include "ns3/point-to-point-net-device.h"
 #include "ns3/vector.h"
 #include "ns3/ipv6-address-generator.h"
+#include "ns3/point-to-point-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/applications-module.h"
 #include "ns3/core-module.h"
 #include "ns3/network-module.h"
-#include "ns3/internet-module.h"
-#include "ns3/point-to-point-module.h"
-#include "ns3/applications-module.h"
+
+NS_LOG_COMPONENT_DEFINE ("DumbbellSimulation");
 
 using namespace ns3;
 
 int main (int argc, char *argv[])
 {
+
+  LogComponentEnable("OnOffApplication", LOG_LEVEL_INFO);
   //Declarations of helpers
   NodeContainer          m_leftLeaf;
   NetDeviceContainer     m_leftLeafDevices;
@@ -50,11 +57,6 @@ int main (int argc, char *argv[])
   Ipv4InterfaceContainer m_rightLeafInterfaces;
   Ipv4InterfaceContainer m_rightRouterInterfaces;
   Ipv4InterfaceContainer m_routerInterfaces;
-  Ipv6InterfaceContainer m_leftLeafInterfaces6;
-  Ipv6InterfaceContainer m_leftRouterInterfaces6;
-  Ipv6InterfaceContainer m_rightLeafInterfaces6;
-  Ipv6InterfaceContainer m_rightRouterInterfaces6;
-  Ipv6InterfaceContainer m_routerInterfaces6;
 
   //Point to Point helpers
   PointToPointHelper longFlowPtP;
@@ -62,17 +64,20 @@ int main (int argc, char *argv[])
   PointToPointHelper pointToPointBottleneck;
   pointToPointBottleneck.SetDeviceAttribute  ("DataRate", StringValue ("10Mbps"));
   pointToPointBottleneck.SetChannelAttribute ("Delay", StringValue ("10ms"));
-  longFlowPtP.setDeviceAttribute ("DataRate", StringValue ("100Mbps"));
-  shortFlowPtP.setDeviceAttribute ("DataRate", StringValue ("100Mbps"));
-  longFlowPtP.setChannelAttribute ("Delay", StringValue ("50ms"));
-  shortFlowPtP.setChannelAttribute ("Delay", StringValue ("5ms"));
+  longFlowPtP.SetDeviceAttribute ("DataRate", StringValue ("100Mbps"));
+  shortFlowPtP.SetDeviceAttribute ("DataRate", StringValue ("100Mbps"));
+  longFlowPtP.SetChannelAttribute ("Delay", StringValue ("50ms"));
+  shortFlowPtP.SetChannelAttribute ("Delay", StringValue ("5ms"));
+
+  uint32_t LeftCount = 6;
+  uint32_t RightCount = 6;
 
   // Create bottleneck routers
   m_routers.Create (2);
 
   // Create the leaf nodes
-  m_leftLeaf.Create (6);
-  m_rightLeaf.Create (6);
+  m_leftLeaf.Create (LeftCount);
+  m_rightLeaf.Create (RightCount);
 
   // Left long links
   for (uint32_t i = 0; i < 3; ++i)
@@ -81,6 +86,9 @@ int main (int argc, char *argv[])
                                                  m_leftLeaf.Get (i));
       m_leftRouterDevices.Add (c.Get (0));
       m_leftLeafDevices.Add (c.Get (1));
+
+      NS_LOG_INFO("The ")
+      NS_LOG_INFO(m_leftLeaf.Get(i))
     }
 
   // Left short links
@@ -114,5 +122,63 @@ int main (int argc, char *argv[])
   stack.Install (m_routers);
   stack.Install (m_leftLeaf);
   stack.Install (m_rightLeaf);
+
+  //Assign IP addresses
+  Ipv4AddressHelper leftIp = Ipv4AddressHelper ("10.1.1.0", "255.255.255.0");
+  Ipv4AddressHelper rightIp = Ipv4AddressHelper ("10.2.1.0", "255.255.255.0");
+  Ipv4AddressHelper routerIp = Ipv4AddressHelper ("10.3.1.0", "255.255.255.0");
+  // Assign the router network
+  m_routerInterfaces = routerIp.Assign (m_routerDevices);
+  // Assign to left side 
+  for (uint32_t i = 0; i < LeftCount; ++i)
+    {
+      NetDeviceContainer ndc;
+      ndc.Add (m_leftLeafDevices.Get (i));
+      ndc.Add (m_leftRouterDevices.Get (i));
+      Ipv4InterfaceContainer ifc = leftIp.Assign (ndc);
+      m_leftLeafInterfaces.Add (ifc.Get (0));
+      m_leftRouterInterfaces.Add (ifc.Get (1));
+      leftIp.NewNetwork ();
+    }
+  // Assign to right side
+  for (uint32_t i = 0; i < RightCount; ++i)
+    {
+      NetDeviceContainer ndc;
+      ndc.Add (m_rightLeafDevices.Get (i));
+      ndc.Add (m_rightRouterDevices.Get (i));
+      Ipv4InterfaceContainer ifc = rightIp.Assign (ndc);
+      m_rightLeafInterfaces.Add (ifc.Get (0));
+      m_rightRouterInterfaces.Add (ifc.Get (1));
+      rightIp.NewNetwork ();
+    }
+
+  NS_LOG_INFO("Setting up simulation!");
+  //Set up simulation
+  OnOffHelper clientHelper ("ns3::TcpSocketFactory", Address ());
+  clientHelper.SetAttribute ("OnTime", StringValue ("ns3::UniformRandomVariable"));
+  clientHelper.SetAttribute ("OffTime", StringValue ("ns3::UniformRandomVariable"));
+  ApplicationContainer clientApps;
+
+  for (uint32_t i = 0; i < RightCount; ++i)
+    {
+      // Create an on/off app sending packets to the same leaf right side
+      AddressValue remoteAddress (InetSocketAddress (m_leftLeafInterfaces.GetAddress (i), 1000));
+      clientHelper.SetAttribute ("Remote", remoteAddress);
+      clientApps.Add (clientHelper.Install (m_rightLeaf.Get (i)));
+    }
+
+  clientApps.Start (Seconds (0.0));
+  clientApps.Stop (Seconds (10.0));
+
+  //Run simulation
+  //
+  NS_LOG_INFO("Starting simulation!");
+  
+  Ipv4GlobalRoutingHelper::PopulateRoutingTables ();
+
+  Simulator::Run ();
+  Simulator::Destroy ();
+
+  NS_LOG_INFO("Simulation complete.");
   return 0;
 }
